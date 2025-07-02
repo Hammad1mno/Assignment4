3cd582cf80072c31221c22f76c6081009f1fdafe
Hammad1mno <hammad17x@gmail.com>
2025-07-02 13:42:56 +0500

    initial commit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+git init
+git branch -m develop 
+mkdir ./src ./styles ./asserts ./bin  ./node_modules 
+touch ./.gitignore ./src/index.html ./styles/index.css
+
+git config --local user.name "Hammad1mno"
+git config --local user.email "hammad17x@gmail.com"
+git config --local core.editor "notepad"
+git remote add origin https://github.com/Hammad1mno/Assignment4.git
